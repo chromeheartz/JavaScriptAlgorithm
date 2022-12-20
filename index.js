72a077e392651f6881f83c7cd33f3a6fda4e7467
bibiboy <95161113+chromeheartz@users.noreply.github.com>
2022-12-21 00:19:18 +0900

    [Ch 07-1] 해시 테이블

--- index.js
+++ index.js
@@ -1,52 +1,13 @@
-// **** JavaScript에서의 사용법 ****
 
-// * Array로 구현 *
-class Queue {
-  constructor(maxSize) {
-    this.maxSize = maxSize;
-    this.queue = [];
-    this.front = 0;
-    this.rear = 0;
-    this.size = 0;
-  }
-
-  enqueue(value) {
-    if (this.isFull()) {
-      console.log("Queue is Full");
-      return;
-    }
-    this.queue[this.rear] = value;
-    this.rear = (this.rear + 1) % this.maxSize;
-    this.size += 1;
-  }
-
-  dequeue() {
-    const value = this.queue[this.front];
-    delete this.queue[this.front];
-    this.front = (this.front + 1) % this.maxSize;
-    this.size -= 1;
-    return value;
-  }
-
-  isFull() {
-    return this.size === this.maxSize;
-  }
-
-  peek() {
-    return this.queue[this.front];
-  }
-}
-
-const queue = new Queue(4);
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(4);
-queue.enqueue(8);
-queue.enqueue(16); // Queue is Full;
-console.log(queue.dequeue()); // 1 
-console.log(queue.dequeue()); // 2
-console.log(queue.size); // 2
-console.log(queue.peek()); // 4
-queue.enqueue(16);
-queue.enqueue(32);
-console.log(queue.isFull()); // true
+// Set
+const tableSet = new Set();
+tableSet.add("key"); // Key와 Value가 동일하게 들어간다.
+tableSet.add("key2");
+console.log(tableSet.has("key")); // true
+console.log(tableSet.has("key3")); // false
+tableSet.delete("key2"); 
+console.log(tableSet.has("key2")); // false
+tableSet.add("key3");
+console.log(tableSet.size); // 2
+tableSet.clear();
+console.log(tableSet.size); // 0
