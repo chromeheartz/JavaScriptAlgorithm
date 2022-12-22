79e5dc8f5f5386ed7aba132d649ff26dd85b9321
chromeheartz <barnesquiat@gmail.com>
2022-12-23 01:42:50 +0900

    [Ch 07-3] 해시테이블 베스트앨범 문제풀이

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 
-// Set
-const tableSet = new Set();
-tableSet.add("key"); // Key와 Value가 동일하게 들어간다.
-tableSet.add("key2");
-console.log(tableSet.has("key")); // true
-console.log(tableSet.has("key3")); // false
-tableSet.delete("key2"); 
-console.log(tableSet.has("key2")); // false
-tableSet.add("key3");
-console.log(tableSet.size); // 2
-tableSet.clear();
-console.log(tableSet.size); // 0
+function solution(genres, plays) {
+  const genreMap = new Map();
+
+  genres
+    .map((genre, index) => [genre, plays[index]])
+    // .forEach((zip,index) => {
+    //   const genre = zip[0];
+    //   const play = zip[1]
+    // })
+    .forEach(([ genre, play ],index) => {
+      const data = genreMap.get(genre) || { total: 0, songs: []};
+      genreMap.set(genre, {
+        total: data.total + play,
+        songs: [...data.songs, { play, index }]
+          .sort((a,b) => b.play - a.play)
+          .slice(0, 2)
+      })
+    })
+
+  console.log([...genreMap.entries()])
+}
+
+
+solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500])
